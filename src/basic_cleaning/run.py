bd259ee975ef60edbe640bbc636badf9cbb31623
mnehete32 <mnehete32@gmail.com>
2024-08-27 19:59:39 -0400

    adding fix

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -39,8 +39,8 @@ def go(args):
     # # Convert last_review to datetime
     # df['last_review'] = pd.to_datetime(df['last_review'])
 
-    # idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
-    # df = df[idx].copy()
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
 
     with tempfile.TemporaryDirectory() as temp_dir:
         output_artifact_path = os.path.join(temp_dir, args.output_artifact)
